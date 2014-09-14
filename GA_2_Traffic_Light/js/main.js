d788b1271e22c52143698544acfd58186cb19237
Stephanie Hom <steph@5deadly.com>
2014-09-14 18:04:25 -0400

    Added wheels and skewing.

--- GA_2_Traffic_Light/js/main.js
+++ GA_2_Traffic_Light/js/main.js
@@ -1,13 +1,18 @@
 function directions() {
-	document.getElementById('directions').innerHTML = 'Click a button to control the traffic light.';
+	document.getElementById('directions').innerHTML = 'Click a button to control the traffic light and car.';
 }
 
 function lightStop() {
 	lightsOff();
 	buttonsOn();
-	document.getElementById('light-stop').style.background = 'rgb(225,86,86';
+	document.getElementById('light-stop').style.background = 'rgb(225,86,86)';
 	document.getElementById('button-stop').disabled = true;
-	document.getElementById('car').style.WebkitAnimationDuration = '2s';
+	document.getElementById('car').style.width = '100px';
+	document.getElementById('car').style.Transform = 'skew(0deg)';
+	document.getElementById('car').style.AnimationPlayState = 'paused';
+	document.getElementById('car').style.MozTransform = 'skew(0deg)';
+	document.getElementById('car').style.MozAnimationPlayState = 'paused';
+	document.getElementById('car').style.WebkitTransform = 'skew(0deg)';
 	document.getElementById('car').style.WebkitAnimationPlayState = 'paused';
 }
 
@@ -16,7 +21,12 @@ function lightCaution() {
 	buttonsOn();
 	document.getElementById('light-caution').style.background = 'rgb(236,244,12)';
 	document.getElementById('button-caution').disabled = true;
-	document.getElementById('car').style.WebkitAnimationDuration = '30s';
+	document.getElementById('car').style.width = '100px';
+	document.getElementById('car').style.Transform = 'skew(0deg)';
+	document.getElementById('car').style.AnimationPlayState = 'running';
+	document.getElementById('car').style.MozTransform = 'skew(0deg)';
+	document.getElementById('car').style.MozAnimationPlayState = 'running';
+	document.getElementById('car').style.WebkitTransform = 'skew(0deg)';
 	document.getElementById('car').style.WebkitAnimationPlayState = 'running';
 }
 
@@ -25,7 +35,12 @@ function lightGo() {
 	buttonsOn();
 	document.getElementById('light-go').style.background = 'rgb(99,197,73)';
 	document.getElementById('button-go').disabled = true;
-	document.getElementById('car').style.WebkitAnimationDuration = '2s';
+	document.getElementById('car').style.width = '120px';
+	document.getElementById('car').style.Transform = 'skew(20deg)';
+	document.getElementById('car').style.AnimationPlayState = 'running';
+	document.getElementById('car').style.MozTransform = 'skew(20deg)';
+	document.getElementById('car').style.MozAnimationPlayState = 'running';
+	document.getElementById('car').style.WebkitTransform = 'skew(20deg)';
 	document.getElementById('car').style.WebkitAnimationPlayState = 'running';
 }
 
