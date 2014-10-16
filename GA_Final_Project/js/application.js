d71f128c2b58383a019e2863dc426df6bd9c5a8d
Stephanie Hom <steph@5deadly.com>
2014-10-16 14:42:13 -0400

    Added question items as objects

---
+++ GA_Final_Project/js/application.js
@@ -0,0 +1,21 @@
+// Each game item and its attributes
+var item1 = {
+	"item" : "I'll <span class="highlight">definately</span> give you a call on Monday afternoon.",
+	"points" : 10,
+	"answer" : false,
+	"issue"	: "spelling"
+}
+
+var item2 = {
+	"item" : "<span class="highlight">It's</span> getting hot in here",
+	"points" : 10,
+	"answer" : true,
+	"issue"	: "possessive"
+}
+
+var item3 = {
+	"item" : "Sam ordered a sandwich, chips and soda.",
+	"points" : 10,
+	"answer" : false,
+	"issue"	: "commas"
+}
