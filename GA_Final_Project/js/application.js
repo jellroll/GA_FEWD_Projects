c37ad726d1115d2948f13ea16cfe83b379fc6e95
Stephanie Hom <steph@5deadly.com>
2014-10-19 09:57:32 -0400

    Added intro, display item, icon font

--- GA_Final_Project/js/application.js
+++ GA_Final_Project/js/application.js
@@ -1,21 +1,87 @@
 // Each game item and its attributes
-var item1 = {
-	"item" : "I'll <span class="highlight">definately</span> give you a call on Monday afternoon.",
-	"points" : 10,
+var item0 = {
+	"stem" : "I'll <span class=\"highlight\">definately</span> give you a call on Monday afternoon.",
 	"answer" : false,
-	"issue"	: "spelling"
+	"fix" : true
+}
+
+var item1 = {
+	"stem" : "It's getting hot in here",
+	"answer" : true,
+	"fix" : true
 }
 
 var item2 = {
-	"item" : "<span class="highlight">It's</span> getting hot in here",
-	"points" : 10,
-	"answer" : true,
-	"issue"	: "possessive"
+	"stem" : "I don't want to loose my spare change.",
+	"answer" : false,
+	"fix" : true
 }
 
 var item3 = {
-	"item" : "Sam ordered a sandwich, chips and soda.",
-	"points" : 10,
+	"stem" : "Sam ordered a sandwich, chips and soda.",
 	"answer" : false,
-	"issue"	: "commas"
+	"fix" : true
+}
+
+// Global variables
+var items = [item0, item1, item2, item3];
+var currentItem = items[0];
+
+
+// Start the game
+function startGame(){
+	document.getElementById("intro").style.display = "none";
+	document.getElementById("card").style.display = "block";
+
+	displayItem();
+}
+
+// Display the item
+function displayItem() {
+	var item = document.getElementById('item');
+	item.innerHTML = currentItem.stem;
+}
+
+
+/*function nextItem() {
+	for (var i = 0; i < items.length; i++) {
+	currentItem = items[i].stem;}
+}*/
+
+
+
+// Evaluate response
+function evaluateResponse() {
+	//console.log(currentItem.answer);
+
+}
+
+
+// Display fix response
+function displayFix() {
+
+}
+
+
+// Display feedback to the fix response
+function displayFeedback() {
+
+}
+
+
+// Tally score
+function tallyScore() {
+
+}
+
+
+// Display results
+function displayResults() {
+
+}
+
+
+// Reset game
+function reset() {
+
 }
